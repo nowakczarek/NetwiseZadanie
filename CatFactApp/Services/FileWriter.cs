3754d05567e98c81b36ef00d0ecf48dce259b858
nowakczarek <czarekkawon@onet.pl>
2025-07-17 21:25:54 +0200

    feat: add FileWriter service

---
+++ CatFactApp/Services/FileWriter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CatFactApp.Models;
+using Microsoft.Extensions.Logging;
+
+namespace CatFactApp.Services
+{
+    public class FileWriter : IFileWriter
+    {
+        private const string FileName = "CatFacts.txt";
+        private readonly ILogger<FileWriter> _logger;
+
+        public FileWriter(ILogger<FileWriter> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task WriteFactToFileAsync(CatFact fact)
+        {
+            try
+            {
+                var line = $"{DateTime.Now:dd-MM-yyyy HH:mm:ss} - {fact.Fact}";
+                var path = Path.Combine(AppContext.BaseDirectory, FileName);
+
+                await File.AppendAllTextAsync(FileName, line + Environment.NewLine);
+
+                _logger.LogInformation("Zapisano fakt w pliku: {path}", path);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Nie udało sie zapisać faktu do pliku");
+            }
+
+        }
+    }
+}
