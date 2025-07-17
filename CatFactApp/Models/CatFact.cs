365d3b91dc9f2a9eeb2ace8a24c0bfde767372a1
nowakczarek <czarekkawon@onet.pl>
2025-07-17 20:53:33 +0200

    feat: add CatFact model using record

---
+++ CatFactApp/Models/CatFact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CatFactApp.Models
+{
+    public record CatFact(
+    [property: JsonPropertyName("fact")] string Fact,
+    [property: JsonPropertyName("length")] int Length
+    );
+}
