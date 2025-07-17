e6a42486120e802e562c6641d335814c7cc8c415
nowakczarek <czarekkawon@onet.pl>
2025-07-17 22:01:11 +0200

    feat: implement main program

--- CatFactApp/Program.cs
+++ CatFactApp/Program.cs
@@ -1,5 +1,8 @@
 ﻿using CatFactApp.Extensions;
+using CatFactApp.Services;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 var builder = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
@@ -8,4 +11,26 @@ var builder = Host.CreateDefaultBuilder(args)
     });
 
 var host = builder.Build();
-Console.WriteLine("App started.");
+
+var logger = host.Services.GetRequiredService<ILogger<Program>>();
+logger.LogInformation("Application started.");
+
+var catFactService = host.Services.GetRequiredService<ICatFactService>();
+var fileWriter = host.Services.GetRequiredService<IFileWriter>();
+
+var catFact = await catFactService.GetCatFactAsync();
+
+
+
+if (catFact is not null)
+{
+    await fileWriter.WriteFactToFileAsync(catFact);
+    logger.LogInformation("Udało się pobrać i zapisać fakt o kocie");
+}
+else
+{
+    logger.LogError("Nie udało się pobrać i zapisać faktu o kocie");
+}
+
+
+
